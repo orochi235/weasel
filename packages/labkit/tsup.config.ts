89a7abb62b17424b20506fad482e097f98430fdd
Michael Baker <contact@michaelbaker.tech>
2026-04-26 23:01:10 -0400

    Add tsup config for library build

---
+++ packages/labkit/tsup.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: {
+    index: 'src/index.ts',
+    'primitives/index': 'src/primitives/index.ts',
+  },
+  format: ['esm'],
+  dts: true,
+  sourcemap: true,
+  clean: true,
+  external: ['react', 'react-dom'],
+  splitting: false,
+  treeshake: true,
+});
