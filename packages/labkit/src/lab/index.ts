e26c4b092862ffc360055659e59175f1bcfd5957
Michael Baker <contact@michaelbaker.tech>
2026-04-26 23:33:21 -0400

    Wire public entry points for lab/ and primitives/

---
+++ packages/labkit/src/lab/index.ts
@@ -0,0 +1,6 @@
+export { LabShell } from './LabShell';
+export type { LabShellProps, LabTheme } from './LabShell';
+export { WorkspaceGrid } from './WorkspaceGrid';
+export type { WorkspaceGridProps } from './WorkspaceGrid';
+export { gridDims } from './gridDims';
+export type { GridDims } from './gridDims';
---
+++ packages/labkit/src/primitives/index.ts
@@ -0,0 +1,9 @@
+export { FpsMeter } from './FpsMeter';
+export { ScaleIndicator } from './ScaleIndicator';
+export type { ScaleIndicatorProps } from './ScaleIndicator';
+export { Sidebar } from './Sidebar';
+export type { SidebarProps } from './Sidebar';
+export { StatusBar } from './StatusBar';
+export type { StatusBarProps } from './StatusBar';
+export { Toolbar } from './Toolbar';
+export type { ToolbarProps } from './Toolbar';
