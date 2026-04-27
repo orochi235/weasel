9ee5b9e32dc2c79c11f45579587097dd77a3e0f2
Michael Baker <contact@michaelbaker.tech>
2026-04-26 23:02:02 -0400

    Add Vite config aliased at @labkit/react

---
+++ packages/labkit/vite.config.ts
@@ -0,0 +1,12 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [react()],
+  root: 'examples',
+  resolve: {
+    alias: {
+      '@labkit/react': new URL('./src/index.ts', import.meta.url).pathname,
+    },
+  },
+});
