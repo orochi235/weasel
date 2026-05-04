5118ddab85188b66f74e9be3422f466bc4497105
Michael Baker <contact@michaelbaker.tech>
2026-05-03 20:03:26 -0400

    feat(tools): export useHandTool from builtin barrel

--- src/tools/builtin/index.ts
+++ src/tools/builtin/index.ts
@@ -4,3 +4,4 @@ export { useUndoRedoTool, type UseUndoRedoToolOptions } from './useUndoRedoTool'
 export { useDuplicateTool, type UseDuplicateToolOptions } from './useDuplicateTool';
 export { useInsertTool, type UseInsertToolOptions } from './useInsertTool';
 export { useSelectTool, type UseSelectToolOptions } from './useSelectTool';
+export { useHandTool } from './useHandTool';
