eb717162e12e15d36445c30ad9bcbfbfc9046066
Michael Baker <contact@michaelbaker.tech>
2026-05-16 21:47:58 -0400

    feat(parallax): public exports via feature barrel

---
+++ src/features/parallax/index.ts
@@ -0,0 +1,4 @@
+export { deriveParallaxView } from './deriveParallaxView';
+export type { ParallaxOpts, ScalarOrXY } from './deriveParallaxView';
+export { createParallaxLayer } from './createParallaxLayer';
+export type { CreateParallaxLayerOpts } from './createParallaxLayer';
